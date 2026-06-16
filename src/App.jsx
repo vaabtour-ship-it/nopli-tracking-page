7df6d5e96559fea5aed759054be59aa72b28a5b4
vaabtour-ship-it <vaabtour@gmail.com>
2026-06-16 14:22:44 +0200

    delete logos

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,6 @@ function App() {
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
           <h1>Suivre mon colis</h1>

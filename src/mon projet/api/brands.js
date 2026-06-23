1e79c89a59579b379405b5b677f1f924c5f625d3
vaabtour-ship-it <vaabtour@gmail.com>
2026-06-23 14:26:02 +0200

    test

--- src/mon projet/api/brands.js
+++ src/mon projet/api/brands.js
@@ -1,7 +1,6 @@
-import { brands } from '../data/mockData.js';
-export const brands = [
-  { id: 1, name: "Apple", country: "États-Unis", industry: "Technologie" },
-  { id: 2, name: "Samsung", country: "Corée du Sud", industry: "Électronique" },
-  { id: 3, name: "Nike", country: "États-Unis", industry: "Mode & Sport" },
-  { id: 4, name: "Toyota", country: "Japon", industry: "Automobile" }
-];
+export default function handler(req, res) {
+  res.status(200).json([
+    { id: 1, name: "Apple" },
+    { id: 2, name: "Samsung" }
+  ]);
+}

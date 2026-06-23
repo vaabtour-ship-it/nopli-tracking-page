b22a82fb1d1b197303c395cc5c9d725502f8d040
vaabtour-ship-it <vaabtour@gmail.com>
2026-06-23 11:07:07 +0200

    Mon premier commit API

---
+++ src/mon projet/data/mockData.js
@@ -0,0 +1,9 @@
+export const users = [
+  { id: 1, name: "Alice", role: "Développeuse" },
+  { id: 2, name: "Bob", role: "Designer" }
+];
+
+export const products = [
+  { id: 101, name: "Ordinateur", price: 999 },
+  { id: 102, name: "Souris", price: 49 }
+];

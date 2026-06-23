c5f51046d1dbe940f4fdbb2e3c26760a5d4a6f20
vaabtour-ship-it <vaabtour@gmail.com>
2026-06-23 16:22:14 +0200

    test

--- src/mon projet/data/mockData.js
+++ src/mon projet/data/mockData.js
@@ -8,3 +8,11 @@ export const products = [
   { id: 101, name: "Ordinateur", price: 999 },
   { id: 102, name: "Souris", price: 49 }
 ];
+// Exemple d'appel à ajouter dans ton index.html
+fetch('/api/brands')
+  .then(res => res.json())
+  .then(brands => {
+    console.log("Marques reçues :", brands);
+    // Ici, tu peux créer des éléments HTML pour les afficher dans ta page
+  })
+  .catch(error => console.error("Erreur marques :", error));

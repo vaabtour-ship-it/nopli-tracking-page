3f4bf8034aa14d52ef18540622a31259eba1f2cb
vaabtour-ship-it <vaabtour@gmail.com>
2026-06-23 15:38:07 +0200

    Mon premier commit API

--- src/mon projet/api/marques.js
+++
@@ -1,7 +0,0 @@
-import { brands } from '../data/mockData.js';
-export const brands = [
-  { id: 1, name: "Apple", country: "États-Unis", industry: "Technologie" },
-  { id: 2, name: "Samsung", country: "Corée du Sud", industry: "Électronique" },
-  { id: 3, name: "Nike", country: "États-Unis", industry: "Mode & Sport" },
-  { id: 4, name: "Toyota", country: "Japon", industry: "Automobile" }
-];

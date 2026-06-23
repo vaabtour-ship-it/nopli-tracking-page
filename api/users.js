68137a65749f46fe2c647f758e95a7cd039cdbd9
vaabtour-ship-it <vaabtour@gmail.com>
2026-06-23 11:29:13 +0200

    test

---
+++ api/users.js
@@ -0,0 +1,6 @@
+import { users } from '../data/mockData.js';
+
+export default function handler(request, response) {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  return response.status(200).json(users);
+}

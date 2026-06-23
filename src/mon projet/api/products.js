3e5ef557f6166be0a9a733121a3f4384935a45de
vaabtour-ship-it <vaabtour@gmail.com>
2026-06-23 11:45:12 +0200

    change products

--- src/mon projet/api/products.js
+++ src/mon projet/api/products.js
@@ -1,6 +1,6 @@
-import { products } from '../data/mockData.js';
-
-export default function handler(request, response) {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  return response.status(200).json(products);
-}
+export const brands = [
+  { id: 1, name: "Apple", country: "États-Unis", industry: "Technologie" },
+  { id: 2, name: "Samsung", country: "Corée du Sud", industry: "Électronique" },
+  { id: 3, name: "Nike", country: "États-Unis", industry: "Mode & Sport" },
+  { id: 4, name: "Toyota", country: "Japon", industry: "Automobile" }
+];

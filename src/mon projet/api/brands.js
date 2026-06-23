73d92f7e4ea10a7079aa5b7c0cff2fc0677504e8
vaabtour-ship-it <vaabtour@gmail.com>
2026-06-23 11:48:39 +0200

    add

--- src/mon projet/api/brands.js
+++ src/mon projet/api/brands.js
@@ -1,3 +1,4 @@
+import { brands } from '../data/mockData.js';
 export const brands = [
   { id: 1, name: "Apple", country: "États-Unis", industry: "Technologie" },
   { id: 2, name: "Samsung", country: "Corée du Sud", industry: "Électronique" },
